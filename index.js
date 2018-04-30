95baf2f966c8c647e41caf2c16f7eda4489adf73
vip522681351 <522681351@qq.com>
2018-05-01 07:42:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ Example: node index.js playback '/SOME/COOL/DIR/pubg-game1.pcap' | pino
 }
 
 function startWebServer () {
-  const apiServerPort = 88
+  const apiServerPort = 80
   backend.listen(apiServerPort, () => {
     console.log('Scientific Chicken Dinner listening on http://localhost:' + apiServerPort)
   })
